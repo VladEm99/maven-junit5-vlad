ac7825bcf2bdb8713b1e84d9da089e4a6e28d9a7
emvladislav <em.vladichek@gmail.com>
2024-02-10 12:25:00 +0200

    added anotations for CalculatorTest tests

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,9 +9,27 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class AppTest {
 
+    private Calculator calculator;
+
+    @BeforeAll
+    static void setUpForAllTests(){
+        System.out.println("Setting up ALL");
+    }
+    @AfterAll
+    static void tearDownForAllTests(){
+        System.out.println("Closing up ALL");
+    }
+    @BeforeEach void setUp(){
+        calculator = new Calculator();
+        System.out.println("Setting up calculator object for new test");
+    }
+    @AfterEach void tearDown(){
+        System.out.println("This is execution of post condition after each test");
+    }
+
     @Test
     public void checkTwoValuesEquals(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals( 4 , calculator.calculateSumOfTwoNumbers(2, 2));
     }
 
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,74 +1,93 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeAll
+    static void setUpForAllTests(){
+        System.out.println("Setting up ALL");
+    }
+    @AfterAll
+    static void tearDownForAllTests(){
+        System.out.println("Closing up ALL");
+    }
+    @BeforeEach
+    void setUp(){
+        calculator = new Calculator();
+        System.out.println("Setting up calculator object for new test");
+    }
+    @AfterEach
+    void tearDown(){
+        System.out.println("This is execution of post condition after each test");
+    }
 
     @Test
     public void testAdditionTwoPositiveIntegersReturnsCorrectResult(){
-        Calculator calculator = new Calculator();
+       // Calculator calculator = new Calculator();
         assertEquals( 4 , calculator.calculateSumOfTwoNumbers(2, 2));
     }
     @Test
     public void testAdditionTwoNegativeIntegersReturnsCorrectResult(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals( -4 , calculator.calculateSumOfTwoNumbers(-2, -2));
     }
     @Test
     public void testAdditionZeroAndPositiveIntegerReturnsCorrectResult(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals( 2 , calculator.calculateSumOfTwoNumbers(0, 2));
     }
     @Test
     public void testAdditionZeroAndNegativeIntegerReturnsCorrectResult(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals( -2 , calculator.calculateSumOfTwoNumbers(0, -2));
     }
     @Test
     public void testSubtractionTwoPositiveIntegersReturnsCorrectResult(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals( 0 , calculator.calculateSubtractionOfTwoNumbers(2, 2));
     }
     @Test
     public void testSubtractionTwoNegativeIntegersReturnsCorrectResult(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals( 0 , calculator.calculateSubtractionOfTwoNumbers(-2, -2));
     }
     @Test
     public void testSubtractionZeroAndPositiveIntegerReturnsCorrectResult(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals( -2 , calculator.calculateSubtractionOfTwoNumbers(0, 2));
     }
     @Test
     public void testSubtractionZeroAndNegativeIntegerReturnsCorrectResult(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals( 2 , calculator.calculateSubtractionOfTwoNumbers(0, -2));
     }
     @Test
     public void testComparisonTwoPositiveIntegersReturnsCorrectResult(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals( true , calculator.checkIfFirstIntegerIsGreater(7, 2));
     }
     @Test
     public void testComparisonTwoNegativeIntegersReturnsCorrectResult(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals( true , calculator.checkIfFirstIntegerIsGreater(-2, -5));
     }
     @Test
     public void testComparisonZeroAndPositiveIntegerReturnsCorrectResult(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals( false , calculator.checkIfFirstIntegerIsGreater(0, 2));
     }
     @Test
     public void testComparisonZeroAndNegativeIntegerReturnsCorrectResult(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals( true , calculator.checkIfFirstIntegerIsGreater(0, -2));
     }
     @Test
     public void testComparisonTwoEqualIntegersReturnsCorrectResult(){
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         assertEquals( true , calculator.checkIfFirstIntegerIsGreater(0, -2));
     }
 }
